cb63e110c12892855e839eacf083bc731b7f26c6
retor <retor@mail.ru>
2014-03-02 14:18:02 +0400

    Clear) And add PagerSlidingTabStrip

--- src/com/retor/ViewPager_UI_tests/HomeActivity.java
+++ src/com/retor/ViewPager_UI_tests/HomeActivity.java
@@ -2,46 +2,50 @@ package com.retor.ViewPager_UI_tests;
 
 import android.app.ActionBar;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.PagerTitleStrip;
 import android.support.v4.view.ViewPager;
+import android.widget.ListView;
+import com.astuetz.PagerSlidingTabStrip;
+
+import java.util.ArrayList;
 
 
 public class HomeActivity extends FragmentActivity {
     /**
      * Called when the activity is first created.
      */
-    ViewPager viewPager;
-    MyPageAdapter myPageAdapter;
+    TestAdapter ta;
+    ArrayList<Fragment> fragments;
 
+    ListViewFragment frag1;
+    ListViewFragment frag2;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        viewPager = new ViewPager(this);
-        viewPager.setId(R.id.viewpager);
-        viewPager.setAdapter(myPageAdapter);
-        setContentView(viewPager);
-        //PagerTitleStrip pagerTitleStrip = (PagerTitleStrip)findViewById(R.id.pagerTitleStrip);
-
-
+        setContentView(R.layout.main);
         final ActionBar bar = getActionBar();
+        ViewPager viewPager = (ViewPager)findViewById(R.id.viewpager);
+        PagerSlidingTabStrip pagerSlidingTabStrip = (PagerSlidingTabStrip)findViewById(R.id.tabs);
+        pagerSlidingTabStrip.setIndicatorHeight(5);
+        pagerSlidingTabStrip.setIndicatorColor(073065);
+        pagerSlidingTabStrip.setTextSize(15);
+        frag1 = new ListViewFragment();
+        frag2 = new ListViewFragment();
+        fragments = new ArrayList<Fragment>();
+
+        fragments.add(0, frag1);
+        fragments.add(1, frag2);
+
+        ta = new TestAdapter(this, getSupportFragmentManager(), fragments);
+
+        viewPager.setAdapter(ta);
+        viewPager.setCurrentItem(0);
+        pagerSlidingTabStrip.setViewPager(viewPager);
         //bar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-        bar.setDisplayOptions(0, ActionBar.DISPLAY_SHOW_TITLE);
-
-        myPageAdapter = new MyPageAdapter(this.getSupportFragmentManager(), this, viewPager);
-        myPageAdapter.addTab(ListViewFragment.class, null);
-        myPageAdapter.addTab(ListViewFragment.class, null);
-
-/*        myPageAdapter.addTab(bar.newTab().setText("List"),
-                FragmentPagerSupport.ArrayListFragment.class, null);
-        mTabsAdapter.addTab(bar.newTab().setText("Cursor"),
-                CursorFragment.class, null);*/
-
-        if (savedInstanceState != null) {
-            bar.setSelectedNavigationItem(savedInstanceState.getInt("tab", 0));
-        }
+        //bar.setDisplayOptions(0, ActionBar.DISPLAY_SHOW_TITLE);
     }
 
     @Override
---
+++ src/com/retor/ViewPager_UI_tests/TestAdapter.java
@@ -0,0 +1,54 @@
+package com.retor.ViewPager_UI_tests;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+
+/**
+ * Created by retor on 01.03.14.
+ */
+public class TestAdapter extends FragmentPagerAdapter {
+    Context context;
+    FragmentManager fm;
+    ArrayList<Fragment> fragments;
+
+    public TestAdapter(Context _context, FragmentManager _fm, ArrayList<Fragment> _fragments){
+        super(_fm);
+        context = _context;
+        fm = _fm;
+        fragments = new ArrayList<Fragment>();
+        fragments = _fragments;
+    }
+
+    public TestAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        return fragments.get(i);
+       // return fm.getFragment();
+    }
+
+    @Override
+    public int getCount() {
+        return fragments.size();
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 0:
+                return "Title One";
+            case 1:
+                return "Title Two";
+            default:
+                return null;
+        }
+    }
+
+}
